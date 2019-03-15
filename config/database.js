40b0f661563edeb2d5f0672eaba3b5b2a0f63438
cracellch <you@example.com>
2019-03-15 15:18:07 -0600

    Conexiones

---
+++ config/database.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+            'password': 'n0m3l0'
+    },
+	'database': 'hola',
+  'users_table':'usuario'
+};
