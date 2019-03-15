4b12a4bd16e3449017632b3793bbe061a8d2d827
cracellch <you@example.com>
2019-03-15 15:12:57 -0600

    EJS

--- app.js
+++ app.js
@@ -3,28 +3,40 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
 var app = express();
-
 // view engine setup
+var server = require('https').Server(app);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./routes/index.js')(app, passport);
+
+app.use((req, res) => res.sendFile("not_found.html", {root: path.join(__dirname, "public")}));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 // error handler
